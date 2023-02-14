1521358b4db4928584239fdfb2743027476f197d
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2023-02-14 14:33:19 -0300

    commit

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,12 @@
+import Loading from "./components/loading.js";
 import React, { lazy, Suspense } from "react";
+
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 const Portifolio = lazy(() => import("./pages/portifolio/index"));
 const FirstContact = lazy(() => import("./pages/firstcontact/index"));
 export function Router() {
   return (
-    <Suspense fallback="Carregando...">
+    <Suspense fallback={<Loading />}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<FirstContact />} />
