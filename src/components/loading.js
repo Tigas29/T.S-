afa53cfe5fa1cd681de33e69bde8d9cd2b42fcf7
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2023-02-14 14:44:32 -0300

    commit

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,40 +1,40 @@
-import { BoxesLoader } from "react-awesome-loaders";
-import { useEffect, useState } from "react";
-import styled from "styled-components";
+// import { BoxesLoader } from "react-awesome-loaders";
+// import { useEffect, useState } from "react";
+// import styled from "styled-components";
 
-const LOADING = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  background-color: #393939;
-  position: fixed;
-  z-index: 99999999999999999999999;
-`;
+// const LOADING = styled.div`
+//   width: 100%;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   height: 100vh;
+//   background-color: #393939;
+//   position: fixed;
+//   z-index: 99999999999999999999999;
+// `;
 
-export default function Loading() {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
-  return (
-    <>
-      {loading ? (
-        <LOADING>
-          <BoxesLoader
-            boxColor={"#985EFF"}
-            style={{ marginBottom: "20px" }}
-            desktopSize={"128px"}
-            mobileSize={"80px"}
-          />
-        </LOADING>
-      ) : (
-        <></>
-      )}
-    </>
-  );
-}
+// export default function Loading() {
+//   const [loading, setLoading] = useState(false);
+//   useEffect(() => {
+//     setLoading(true);
+//     setTimeout(() => {
+//       setLoading(false);
+//     }, 1000);
+//   }, []);
+//   return (
+//     <>
+//       {loading ? (
+//         <LOADING>
+//           <BoxesLoader
+//             boxColor={"#985EFF"}
+//             style={{ marginBottom: "20px" }}
+//             desktopSize={"128px"}
+//             mobileSize={"80px"}
+//           />
+//         </LOADING>
+//       ) : (
+//         <></>
+//       )}
+//     </>
+//   );
+// }
