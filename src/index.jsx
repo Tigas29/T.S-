f3e3055514c0e5c241ebe915b3bc6283b8f45557
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2023-02-15 09:32:28 -0300

    commit

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./reset.css";
 import { Router } from "./router";
+import Loading from "./components/loading";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router />
+    <Loading />
   </React.StrictMode>
 );
